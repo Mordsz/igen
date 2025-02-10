d27c9137a501cfe624873619c2185bfa4b2155d0
Mordsz <petri.aron@simonyiszki.org>
2025-02-10 13:44:55 +0100

    Adatbekérés és konverzió

---
+++ src/hetfodelutan/Hetfodelutan.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hetfodelutan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author petri
+ */
+public class Hetfodelutan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    boolean igaz = true;
+    boolean hamis = false;
+    
+    System.out.println("Kérek egy szöveget: ");
+    Scanner be = new Scanner(System.in);
+    String adat = be.next();
+    System.out.println(adat);
+    
+    System.out.println("Kérek egy egész számot:");
+    int a = be.nextInt();
+    System.out.println("a");
+    
+    be.close();
+    
+            
+    
+    
+    
+    
+    }
+    
+}
